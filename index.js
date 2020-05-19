6fe54809e42b96731a8bb6ee5cb714942106c89a
Zabrina Brodale <zabrinaisis@gmail.com>
2020-05-19 02:05:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -69,7 +69,12 @@ function placeOrder(cardNumber) {
   if (cardNumber === "") {
     return ("Sorry, we don't have a credit card on file for you.");
   } else {
+    var theTotal = 0;
+    var prices = cart.map(element => element.itemPrice);
+    for (let i=0; i<prices.length; i++) {
+      theTotal += prices[i];
+    }
     cart = [];
-    return ("Your total cost is $" + total() + ", which will be charged to the card " + cardNumber + ".");
+    return ("Your total cost is $" + theTotal + ", which will be charged to the card " + cardNumber + ".");
   }
 }
